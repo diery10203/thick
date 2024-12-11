c9105c5ca6da1431f1bedd9d4ff2358059162a2f
H51M14\Student <Student@gmail.com>
2024-12-11 13:13:15 +0700

    finish 1b

--- App.js
+++ App.js
@@ -6,9 +6,34 @@ import {useNavigation} from '@react-navigation/native';
 export default function Screen1(){
   
   return(
-    <View>
-      <Image source={require('./assets/1.png')}/>
-      <Text>Discover Your Home</Text>
+    <View style = {styles.container} >
+      <View>
+        <Image source={require('./assets/1.png')}/>
+        <Text>Discover Your Home</Text>
+      </View>
+
+      <View style= {styles.btn_ls}>
+        <TouchableOpacity style ={styles.btn_log}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style = {styles.btn_log}>
+          <Text>Sign up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems: 'center',
+  },
+  btn_ls:{
+    flexDirection: 'row',
+    padding: 10,
+  },
+  btn_log:{
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+})
