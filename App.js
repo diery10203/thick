5496835ced50faeb297fbad85ab11d2e00664258
H51M14\Student <Student@gmail.com>
2024-12-11 13:53:09 +0700

    finish 2a_1

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {Provider} from "react-redux";
-
+import {store} from "./src/redux/store"
 
 import Screen1 from "./src/pages/Screen1";
 import Screen2 from "./src/pages/Screen2"
@@ -12,12 +12,13 @@ export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
-      
+        <Provider store ={store}>
         <NavigationContainer>
           <Stack.Navigator initialRouteName = "Screen1">
               <Stack.Screen name = "Screen1" component = {Screen1}/>
               <Stack.Screen name = "Screen2" component = {Screen2}/>
           </Stack.Navigator>    
         </NavigationContainer>
+        </Provider>
   );
 }
--- src/pages/Screen2.js
+++ src/pages/Screen2.js
@@ -1,27 +1,145 @@
-import * as React from 'react';
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View, Image, TouchableOpacity,} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import {View,Text,FlatList,TouchableOpacity,Image,StyleSheet} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux'; // Redux hooks
+import { fetchBikes } from '../redux/bikeSlice'; // Gọi từ slice
 
-export default function Screen1(){
-  const navigation = useNavigation();
-  return(
-    <View style = {styles.container} >
+export default function Screen2 (){
+  const dispatch = useDispatch();
+
+  const bikes = useSelector((state) => state.bikes.data); // Lấy danh sách sản phẩm
+
+  const status = useSelector((state) => state.bikes.status);
+
+  const [filter, setFilter] = useState('All'); // Trạng thái bộ lọc
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchBikes());
+    }
+  }, [dispatch, status]);
+  
+  const filteredData = bikes.filter(
+    (
+      item // Thêm logic lọc
+    ) => (filter === 'All' ? true : item.type === filter)
+  );
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Image source={{ uri: item.image }} style={styles.itemImage} />
+      <Text style={styles.itemName}>{item.name || 'No Name'}</Text>
+      <Text style={styles.itemPrice}>
+        {item.price ? `$${item.price}` : 'No Price'}
+      </Text>
+    </View>
+  );
+
+  return (
+    <View style = {styles.container}>
+
+      <View style = {styles.titleContainer}>
+        <Text style = {styles.titleText}>Search</Text>
+      </View>
+
+      <View style = {styles.container_touch}>
+        <TouchableOpacity style={[
+          styles.container_touch1,
+          filter === 'All',
+        ]}
+        onPress={() => setFilter('All')}>
+        <Text style={styles.container_touch1a}>Real Estate</Text>
+        </TouchableOpacity>   
+
+        <TouchableOpacity style={[
+          styles.container_touch1,
+          filter === 'Roadbike', // Highlight nút All,
+        ]}
+          onPress={() => setFilter('Roadbike')}>
+          <Text style={styles.container_touch1a}>Apartment</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[
+          styles.container_touch1,
+          filter === 'Mountain',
+        ]}
+          onPress={() => setFilter('Mountain')}>
+          <Text style={styles.container_touch1a}>House</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[
+          styles.container_touch1,
+          filter === 'Mountain',
+        ]}
+          onPress={() => setFilter('Mountain')}>
+          <Text style={styles.container_touch1a}>Motel</Text>
+        </TouchableOpacity>
+      </View>
      
     </View>
-  )
+  );
 }
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff', 
+  },
+  titleContainer: {
+    marginBottom: 20,
     alignItems: 'center',
   },
-  btn_ls:{
-    flexDirection: 'row',
-    padding: 10,
+  titleText: {
+    fontSize: 25,
+    fontWeight: '700',
   },
-  btn_log:{
+  container_touch: {
+    marginVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  container_touch1: {
+    width: 99,
+    height: 32,
     borderWidth: 1,
     borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#E94141',
+  },
+  container_touch1a: {
+    color: '#E94141',
+    fontSize: 20,
+    fontWeight: '400',
+  },
+  row: {
+    justifyContent: 'space-between', // Căn đều các mục trong hàng
+    marginBottom: 10,
+  },
+  itemContainer: {
+    flex: 1,
+    margin: 5,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    alignItems: 'center',
+    backgroundColor: '#f9f9f9',
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  itemPrice: {
+    fontSize: 14,
+    color: '#888',
   },
 })
